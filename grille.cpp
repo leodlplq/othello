58d2f97acdfd6715cb79397e8f1ca0b72250e147
Léo Delplanque <delplanqueleomail@gmail.com>
2020-12-19 15:32:50 +0100

    init the project

---
+++ grille.cpp
@@ -0,0 +1,10 @@
+//c++ file to create the grid.
+#include <iostream>
+
+using namespace std;
+
+
+
+void renderGrid(char grid){
+
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+char grid[256] = {
+    "+", 
+}
+
+int main(){
+
+    cout << "salut" << endl;
+    return 0;
+}
